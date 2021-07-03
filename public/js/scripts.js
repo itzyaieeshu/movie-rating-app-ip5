543ba11d2d97efeee192a32f89fdf480b6f68c36
itzyaieeshu <itzyaieeshu@gmail.com>
2021-07-04 00:44:03 +1000

    api axios & jquery ajax requests added

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,11 +1,8 @@
 console.log("Testing")
 
-const api_key = 'adbb4f7a391daade032a6f2b704fcf8c'
-const tmdb_base_url = 'https://api.themoviedb.org/3'
-const api_query = `?api_key=${api_key}`
 
 function getPopularMovies() {
-    $.getJSON(`${tmdb_base_url}/discover/movie${api_query}`, function(data) {
+    $.getJSON(`/api/discover/movie`, (data) => {
         console.log(data.results)
     })
 }
--- routes/routes.js
+++ routes/routes.js
@@ -2,12 +2,14 @@ const index = require('./handlers/index');
 const login = require('./handlers/login');
 const signup = require('./handlers/signup');
 const moviesList = require('./handlers/moviesList');
-const movie = require('./handlers/movie')
+const movie = require('./handlers/movie');
+const moviesAPI = require('./services/moviesAPI');
 
 module.exports = (app) => {
     app.get('/', index.page),
     app.get('/login', login.page),
     app.get('/signup', signup.page),
     app.get('/movies', moviesList.page),
-    app.get('/movie', movie.page)
+    app.get('/movie', movie.page),
+    app.get('/api/discover/movie', moviesAPI.popularMovies)
 }
