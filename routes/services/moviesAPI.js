126b01fff0347541badfb4099124cbc1d737d7c5
itzyaieeshu <itzyaieeshu@gmail.com>
2021-07-10 01:11:44 +1000

    index other genre movies list added

--- routes/services/moviesAPI.js
+++ routes/services/moviesAPI.js
@@ -4,6 +4,7 @@ axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 const api_key = process.env.TMDB_API_KEY;
 const api_query = `?api_key=${api_key}`;
 const lang = '&language=en-US';
+const popularity = '&sort_by=popularity.desc';
 
 const popularMovies = () => axios.get('/discover/movie' + api_query);
 
@@ -11,8 +12,11 @@ const movie = (id) => axios.get('/movie/' + id + api_query);
 
 const movieCredits = (id) => axios.get('/movie/' + id + '/credits' + api_query + lang);
 
+const genre = (genreName) => axios.get('/discover/movie' + api_query + popularity + '&with_genres=' + genreName);
+
 module.exports = {
     popularMovies,
     movie,
     movieCredits,
+    genre,
 }
