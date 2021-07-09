e001407bba044cf3078bc76d818af868283f6be0
itzyaieeshu <itzyaieeshu@gmail.com>
2021-07-09 23:59:37 +1000

    movie page update

--- routes/handlers/movie.js
+++ routes/handlers/movie.js
@@ -1,11 +1,13 @@
 const moviesAPI = require('../services/moviesAPI');
 
 const page = (req, res) => {
-  moviesAPI.movie(req.params.id)
-  .then(response => {
+  Promise.all([moviesAPI.movie(req.params.id), moviesAPI.movieCredits(req.params.id), moviesAPI.popularMovies()])
+  .then(results => {
       res.render("pages/movie", {
       title: 'movie',
-      movie: response.data,
+      movie: results[0].data,
+      movieCredits: results[1].data,
+      popularMovies: results[2].data.results,
     });
   })
   .catch(err => console.error(err));
--- routes/services/moviesAPI.js
+++ routes/services/moviesAPI.js
@@ -1,14 +1,18 @@
 const axios = require('axios');
 const querystring = require('querystring');
-axios.defaults.baseURL = 'https://api.themoviedb.org/3'
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 const api_key = process.env.TMDB_API_KEY;
-const api_query = `?api_key=${api_key}`
+const api_query = `?api_key=${api_key}`;
+const lang = '&language=en-US';
 
 const popularMovies = () => axios.get('/discover/movie' + api_query);
 
 const movie = (id) => axios.get('/movie/' + id + api_query);
 
+const movieCredits = (id) => axios.get('/movie/' + id + '/credits' + api_query + lang);
+
 module.exports = {
     popularMovies,
     movie,
+    movieCredits,
 }
