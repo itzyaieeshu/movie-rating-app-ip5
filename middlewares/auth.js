618baca1558f798bc89014a2796b42fcac31fa15
Rumali Siddiqua <rumali.siddiqua@g.bracu.ac.bd>
2021-07-10 13:42:01 +1000

    Middleware authentication

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,61 +1,17 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const { compare, hash } = require('bcryptjs')
+const createError = require('http-errors')
+
+const hashPassword = async (password) => hash(password, 8)
+const comparePassword = async (requestPassword, passwordInDbB) =>
+  compare(requestPassword, passwordInDbB)
+
+const routeProtector = (req, res, next) => {
+  if (req.session && req.session.user) return next()
+  throw createError(403, 'Forbidden')
+}
 
 module.exports = {
-    setAuthToken: (userId, firstName) => {
-        firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
-        loggedIn = true;
-        return new Promise(function (resolve, reject) {
-            jwt.sign(
-                { userId, firstName, loggedIn },
-                'secretAccessKey',
-                // { expiresIn: '10m' },
-                (err, token) => {
-                    if (err) reject(err);
-                    resolve(token);
-                }
-            );
-        });
-    },
-
-    getTokenData: (req, res, next) => {
-        const token = req.cookies['AuthToken'] || req.body['AuthToken'];
-        console.log('(getTokenData) Token', token);
-
-        if (token === undefined) {
-            req.user = {
-                userId: undefined,
-                firstName: undefined,
-                loggedIn: false,
-            };
-            console.log('(getTokenData) is undefined. req.user set to', req.user);
-            next();
-        } else {
-            jwt.verify(token, 'secretAccessKey', (err, user) => {
-                if (err) {
-                    console.log('(getTokenData) jwt.verify err', err);
-                    next(err);
-                } else {
-                    console.log('(getTokenData) user', user);
-                    req.user = user;
-                    next();
-                }
-            });
-        }
-    },
-
-    unsetAuthToken: (req, res, next) => {
-        res.clearCookie('AuthToken');
-        next();
-    },
-
-    getHashedPassword: async (password) => {
-        try {
-            const salt = await bcrypt.genSalt(10);
-            const hash = await bcrypt.hash(password, salt);
-            return hash;
-        } catch (ex) {
-            console.log(ex.message);
-        }
-    },
-};
+  hashPassword,
+  comparePassword,
+  routeProtector,
+}
