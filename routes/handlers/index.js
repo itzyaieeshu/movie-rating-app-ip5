94f723655b6f0c0bd52ba149e0b4f8c65c01e2aa
itzyaieeshu <itzyaieeshu@gmail.com>
2021-07-11 12:29:30 +1000

    index page login / logout switch update

--- routes/handlers/index.js
+++ routes/handlers/index.js
@@ -1,31 +1,24 @@
-const moviesAPI = require('../services/moviesAPI');
+const moviesAPI = require("../services/moviesAPI");
 
 const page = (req, res) => {
-  Promise.all([moviesAPI.popularMovies(), moviesAPI.genre('16'), moviesAPI.genre('878'), moviesAPI.genre('53')])
-  .then(result => {
-    if (!req.session.userId) {
-        res.render("pages/index", {
-          title: 'Home',
-          session: false,
-          sessionUser: '',
-          popularMovies: result[0].data.results,
-          animationMovies: result[1].data.results,
-          scifiMovies: result[2].data.results,
-          thrillerMovies: result[3].data.results,
-      }); 
-    } else {
-        res.render("pages/index", {
-          title: 'Home',
-          session: true,
-          sessionUser: req.session.firstname,
-          popularMovies: result[0].data.results,
-          animationMovies: result[1].data.results,
-          scifiMovies: result[2].data.results,
-          thrillerMovies: result[3].data.results,
-      }); 
-    }
-  })
-  .catch(err => console.error(err));
+  Promise.all([
+    moviesAPI.popularMovies(),
+    moviesAPI.genre("16"),
+    moviesAPI.genre("878"),
+    moviesAPI.genre("53"),
+  ])
+    .then((result) => {
+      res.render("pages/index", {
+        title: "Home",
+        session: !req.session.userId ? false : true,
+        sessionUser: !req.session.userId ? "" : req.session.firstname,
+        popularMovies: result[0].data.results,
+        animationMovies: result[1].data.results,
+        scifiMovies: result[2].data.results,
+        thrillerMovies: result[3].data.results,
+      });
+    })
+    .catch((err) => console.error(err));
 };
 
 module.exports = {
