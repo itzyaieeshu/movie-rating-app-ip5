e87aca33aabde3bc9280ead49cc246ffd385db55
Ashraf <ashrafunnisa931@gmail.com>
2021-07-12 04:14:19 +1000

    Edit movie page to calculate average rating

--- routes/handlers/movie.js
+++ routes/handlers/movie.js
@@ -1,4 +1,5 @@
 const moviesAPI = require("../services/moviesAPI");
+const db = require('../../config/database')
 
 const page = (req, res) => {
   Promise.all([
@@ -7,16 +8,30 @@ const page = (req, res) => {
     moviesAPI.popularMovies(),
   ])
     .then((results) => {
-      res.render("pages/movie", {
-        title: "movie",
-        session: !req.session.userId ? false : true,
-        sessionUser: !req.session.userId ? "" : req.session.firstname,
-        movie: results[0].data,
-        movieCredits: results[1].data,
-        popularMovies: results[2].data.results,
-        message: req.query.message,
-        successMessage: req.query.successMessage,
-      });
+      console.log("in movie page")
+         db.any('SELECT ROUND(AVG(rating),0) FROM ratings where movie_id = $1 GROUP BY movie_id;',[req.params.movieid]) 
+        .then((average) => {
+            console.log("Avg: " + average)
+            console.log("Query for avg rating executed")
+            res.render("pages/movie", {
+              title: "movie",
+              session: !req.session.userId ? false : true,
+              sessionUser: !req.session.userId ? "" : req.session.firstname,
+              movie: results[0].data,
+              movieCredits: results[1].data,
+              popularMovies: results[2].data.results,
+              message: req.query.message,
+              successMessage: req.query.successMessage,
+              averageRating: average
+            });
+          })
+          //Query to get average rating failed
+          .catch(err => {
+            console.log("Query to get average rating failed")
+            res.render("pages/error", {
+                message: err.message + " " + err.query,
+            });
+          })  
     })
     .catch((err) => console.error(err));
 };
