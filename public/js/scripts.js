101c720e367ed45c6be3dd0857bf8c074251fc78
itzyaieeshu <itzyaieeshu@gmail.com>
2021-07-13 02:03:12 +1000

    movie-rating modal added

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,11 @@
-console.log("Testing")
-
+const rateLoginModal = document.getElementById('rateLoginModal');
+const rateMovieModal = document.getElementById('rateMovieModal');
+    const openModal = (modal) => {
+      modal.style.display = 'block';
+    };
+    closeModal = (modal) => {
+      modal.style.display = 'none';
+    };
 const getPopularMovies = () => {
     let results;
     $.getJSON(`/api/discover/movie`, (data) => {
