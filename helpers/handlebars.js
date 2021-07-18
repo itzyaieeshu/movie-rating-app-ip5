b305fbdb1613ec7e8bf983419211d41efe313096
itzyaieeshu <itzyaieeshu@gmail.com>
2021-07-18 21:28:46 +1000

    minor fix on handlebars splitYear function

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -1,6 +1,6 @@
 const expressHandlebars = require('express-handlebars');
 const hbs = expressHandlebars.create({});
-hbs.handlebars.registerHelper("splitYear", (date) => date.slice(0,4));
+hbs.handlebars.registerHelper("splitYear", (date) => (date) ? date.slice(0,4) : '');
 hbs.handlebars.registerHelper("convertToHours", (mins) => {
     let hours = Math.floor(mins/60)
     let min = Math.round(((mins/60)-hours) * 60)
