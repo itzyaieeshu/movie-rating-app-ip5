87ec5fc47604254a8d20c29ebad0f050cfbf8bc6
itzyaieeshu <itzyaieeshu@gmail.com>
2021-07-21 14:06:48 +1000

    search option update frontend

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -18,8 +18,28 @@ const getPopularMovies = () => {
     return results
 }
 const searchMovies = (key) => {
-  $.getJSON(`/api/discover/movie`, (data) => {
-      console.log(data)
+  $.getJSON(`/api/search/movie/` + key, (data) => {
+      $("#search-results").empty()
+    data.results.forEach(movie => {
+          $( "#search-results" ).append(
+            `<div class="col-md-4 pb-4">
+            <a href="/movie/${movie.id}">
+                <div class="card bg-cs-2 shadow-sm p-3">
+                    <div class="col-sm-12 row">
+                        <div class="col-sm-4">
+                            <img
+                                src="https://image.tmdb.org/t/p/original/${movie.poster_path}"
+                                class="card-img-top" alt="...">
+                        </div>
+                        <div class="col-sm-8 text-light">
+                            <h4>${movie.title}</h4>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>`
+          );
+    });
       return data
   })
 }
@@ -28,7 +48,10 @@ getPopularMovies();
 
 document.addEventListener('DOMContentLoaded', () => { 
   searchBox.oninput = () => {
-    searchMovies(searchBox.val)
+    searchMovies(searchBox.value)
+    if(!searchBox.value) {
+      $("#search-results").empty()
+    }
   };
 });
 
